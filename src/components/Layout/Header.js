01052a49231f1c99ffbe80a8bc188a7c3aa46245
Dkboss743 <2019246@iiitdmj.ac.in>
2022-05-13 00:27:12 +0530

    added meals

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import image from "../../Assests/meals.jpg";
+import styles from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={styles.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton></HeaderCartButton>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={image} alt="food" />
+      </div>
+      {props.children}
+    </React.Fragment>
+  );
+};
+export default Header;
