5b1d06caba8b2c6eda64002629cd0118a91fcaec
Dkboss743 <2019246@iiitdmj.ac.in>
2022-05-18 20:19:21 +0530

    uploding order details to firebase database

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,9 +1,11 @@
 import classes from "./Checkout.module.css";
+import { useContext } from "react";
 import { useRef, useState } from "react";
+import CartContext from "../../store/cart-content";
 const isEmpty = (value) => value.trim() === "";
-const isNotFiveChars = (value) => value.trim().length !== 5;
-
+const isNotFiveChars = (value) => value.trim().length !== 6;
 const Checkout = (props) => {
+  const cartCtx = useContext(CartContext);
   const [formInputValidity, setFormInputValidity] = useState({
     name: true,
     street: true,
@@ -14,6 +16,30 @@ const Checkout = (props) => {
   const streetInputRef = useRef();
   const postalCodeInputRef = useRef();
   const cityInputRef = useRef();
+  const sendData = async (userData) => {
+    const OrderData = [];
+    const data = [];
+    cartCtx.item.map((item) => {
+      data.push({
+        key: item.id,
+        name: item.name,
+        amount: item.amount,
+        price: item.price,
+      });
+    });
+    OrderData.push(userData);
+    OrderData.push(data);
+    const sendingData = await fetch(
+      "https://logical-honor-328116-default-rtdb.firebaseio.com/Meals/Order.json",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application.json",
+        },
+        body: JSON.stringify(OrderData),
+      }
+    );
+  };
   const confirmHandler = (event) => {
     event.preventDefault();
     const enteredName = nameInputRef.current.value;
@@ -32,12 +58,20 @@ const Checkout = (props) => {
     });
     const formIsValid =
       enteredCityIsValid &&
-      enteredCityIsValid &&
       enteredNameIsValid &&
       enteredStreetIsValid &&
       enteredPostalCodeIsValid;
     if (!formIsValid) {
       return;
+    }
+    if (formIsValid) {
+      const userData = {
+        name: enteredName,
+        city: enteredCity,
+        Street: enteredStreet,
+        postalCode: enteredPostalCode,
+      };
+      sendData(userData);
     }
   };
   const style = (property) => {
